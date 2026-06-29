84c1c58dd0ad669faacd3e8765cd71a06f925c0c
phjpdev <jeanpatrick97816@gmail.com>
2026-06-30 07:16:31 +0800

    seg

--- frontend/components/layout/ConditionalHeader.tsx
+++ frontend/components/layout/ConditionalHeader.tsx
@@ -4,7 +4,7 @@ import { usePathname } from "next/navigation";
 import { Header } from "./Header";
 import { ROUTES } from "@/lib/constants";
 
-const HIDDEN_HEADER_ROUTES = new Set([ROUTES.WELCOME]);
+const HIDDEN_HEADER_ROUTES: ReadonlySet<string> = new Set([ROUTES.WELCOME]);
 
 export function ConditionalHeader() {
   const pathname = usePathname();
